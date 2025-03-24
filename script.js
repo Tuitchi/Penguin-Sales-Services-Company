3cac88c52bcbb9a6ab80706499970b1b585a5635
Nikolai Lapatan <154252905+Tuitchi@users.noreply.github.com>
2025-03-24 16:11:34 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', function () {
+    const logo = document.getElementById('logo');
+    if (window.scrollY > 50) {
+      logo.classList.remove('w-20');
+      logo.classList.add('w-12');
+    } else {
+      logo.classList.remove('w-12');
+      logo.classList.add('w-20');
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    const services = document.querySelectorAll("#service");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (entry.target.classList.contains("md:flex-row-reverse")) {
+                    entry.target.classList.remove("opacity-0", "-translate-x-10");
+                } else {
+                    entry.target.classList.remove("opacity-0", "translate-x-10");
+                }
+                entry.target.classList.add("opacity-100", "translate-x-0");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    services.forEach(service => {
+        observer.observe(service);
+    });
+});
+
